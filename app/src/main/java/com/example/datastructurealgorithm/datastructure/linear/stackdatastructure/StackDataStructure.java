9b664b777805c558e0d691aaa848d8deec45c936
M.Zareei <m.zareei@jabama.com>
2023-08-21 13:23:45 +0330

    .

--- app/src/main/java/com/example/datastructurealgorithm/datastructure/linear/stackdatastructure/StackDataStructure.java
+++ app/src/main/java/com/example/datastructurealgorithm/datastructure/linear/stackdatastructure/StackDataStructure.java
@@ -1,4 +1,17 @@
 package com.example.datastructurealgorithm.datastructure.linear.stackdatastructure;
 
 public class StackDataStructure {
+    public static void main(String[] args){
+        Stack stack = new Stack(5);
+
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+
+        stack.pop();
+        System.out.println("\nAfter popping out");
+
+        stack.printStack();
+    }
 }
