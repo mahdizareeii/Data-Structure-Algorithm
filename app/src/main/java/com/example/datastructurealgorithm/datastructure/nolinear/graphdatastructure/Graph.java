760022a4dfa281c5f37076e71824be6e630f8d3d
M.Zareei <m.zareei@jabama.com>
2023-08-28 16:22:31 +0330

    Graph data structure

---
+++ app/src/main/java/com/example/datastructurealgorithm/datastructure/nolinear/graphdatastructure/Graph.java
@@ -0,0 +1,47 @@
+package com.example.datastructurealgorithm.datastructure.nolinear.graphdatastructure;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+// Generic Graph class
+class Graph<T> {
+    private int vertices;
+    private Map<Integer, List<T>> adjacencyList;
+
+    // Constructor to initialize the graph with a given number of vertices
+    public Graph(int vertices) {
+        this.vertices = vertices;
+        adjacencyList = new HashMap<>();
+        
+        // Initialize adjacency lists for each vertex
+        for (int i = 0; i < vertices; i++) {
+            // We're using Integer.valueOf(i) as generic keys
+            adjacencyList.put(i, new LinkedList<>());
+        }
+    }
+
+    // Method to add an edge between source and destination vertices
+    public void addEdge(T source, T destination) {
+        // Add destination to source's adjacency list
+        adjacencyList.get(source).add(destination);
+        
+        // For an undirected graph, add source to destination's adjacency list
+        adjacencyList.get(destination).add(source);
+    }
+
+    // Method to print the adjacency list of the graph
+    public void printGraph() {
+        for (Map.Entry<Integer, List<T>> entry : adjacencyList.entrySet()) {
+            Integer vertex = entry.getKey();
+            List<T> neighbors = entry.getValue();
+
+            System.out.print("Vertex " + vertex + " is connected to: ");
+            for (T neighbor : neighbors) {
+                System.out.print(neighbor + " ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ app/src/main/java/com/example/datastructurealgorithm/datastructure/nolinear/graphdatastructure/GraphDataStructure.java
@@ -0,0 +1,41 @@
+package com.example.datastructurealgorithm.datastructure.nolinear.graphdatastructure;
+
+public class GraphDataStructure {
+    public static void main(String[] args) {
+
+
+        // A---B
+        // |  /|
+        // | / |
+        // C---D---E
+
+        // Create a graph with generic vertices (String in this case)
+        Graph<Integer> graph = new Graph<>(5);
+
+        // Add edges to the graph
+        graph.addEdge(1, 2);
+        graph.addEdge(1, 3);
+        graph.addEdge(1, 4);
+        graph.addEdge(2, 4);
+        graph.addEdge(2, 5);
+        graph.addEdge(3, 4);
+        graph.addEdge(3, 5);
+        graph.addEdge(4, 5);
+
+
+        // Print the adjacency list of the graph
+        System.out.println("Graph adjacency list:");
+        graph.printGraph();
+
+        // Adjacency List:
+        // Vertex 0 is connected to: 1 - 2
+        // Vertex 0 is connected to: 1 - 3
+        // Vertex 0 is connected to: 1 - 4
+        // Vertex 0 is connected to: 2 - 4
+        // Vertex 0 is connected to: 2 - 5
+        // Vertex 0 is connected to: 3 - 4
+        // Vertex 0 is connected to: 3 - 5
+        // Vertex 0 is connected to: 4 - 5
+    }
+
+}
