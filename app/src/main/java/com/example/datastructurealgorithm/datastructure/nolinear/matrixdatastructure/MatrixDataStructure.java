5b84a083e4f1d37d4fc78cb258855e30fb51c1a9
M.Zareei <m.zareei@jabama.com>
2023-08-29 11:48:44 +0330

    matrix data structure

--- app/src/main/java/com/example/datastructurealgorithm/datastructure/nolinear/matrixdatastructure/MatrixDataStructure.java
+++ app/src/main/java/com/example/datastructurealgorithm/datastructure/nolinear/matrixdatastructure/MatrixDataStructure.java
@@ -5,15 +5,32 @@ public class MatrixDataStructure {
 
     }
 
-    private void test2dMatrix(){
+    private void test2dMatrix() {
         //*  *  *  *
         //*  *  *  *
         //*  *  *  *
         _2dMatrix matrix = new _2dMatrix(3, 4);
         matrix.addData();
+        matrix.printData();
     }
 
-    private void test3dMatrix(){
+    private void test3dMatrix() {
+        //plane 1
+        //*  *  *  *
+        //*  *  *  *
+        //*  *  *  *
 
+        //plane 2
+        //*  *  *  *
+        //*  *  *  *
+        //*  *  *  *
+
+        //plane 3
+        //*  *  *  *
+        //*  *  *  *
+        //*  *  *  *
+        _3dMatrix matrix = new _3dMatrix(3, 3, 4);
+        matrix.addData();
+        matrix.printData();
     }
 }
