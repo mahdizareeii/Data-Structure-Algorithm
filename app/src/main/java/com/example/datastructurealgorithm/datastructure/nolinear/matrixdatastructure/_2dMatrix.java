c5ef904c35b77dabe0158c9c9edf22491f73ea4e
M.Zareei <m.zareei@jabama.com>
2023-08-29 11:40:50 +0330

    matrix data structure

--- app/src/main/java/com/example/datastructurealgorithm/datastructure/nolinear/matrixdatastructure/_2dMatrix.java
+++ app/src/main/java/com/example/datastructurealgorithm/datastructure/nolinear/matrixdatastructure/_2dMatrix.java
@@ -4,20 +4,20 @@ import java.util.Scanner;
 
 public class _2dMatrix {
 
-    final int rowSize;
-    final int columnSize;
+    final int rowCount;
+    final int columnCount;
     public final int[][] matrix;
 
     public _2dMatrix(int row, int column) {
-        this.rowSize = row;
-        this.columnSize = column;
+        this.rowCount = row;
+        this.columnCount = column;
         matrix = new int[row][column];
     }
 
     public void addData() {
         Scanner scanner = new Scanner(System.in);
-        for (int row = 0; row < rowSize; row++) {
-            for (int column = 0; column < columnSize; column++) {
+        for (int row = 0; row < rowCount; row++) {
+            for (int column = 0; column < columnCount; column++) {
                 System.out.print("row[" + row + "] - column[" + column + "] : ");
                 matrix[row][column] = scanner.nextInt();
             }
@@ -28,7 +28,7 @@ public class _2dMatrix {
     public void printData() {
         for (int[] row : matrix) {
             for (int value : row) {
-                System.out.print(" " + value);
+                System.out.print(value + " ");
             }
             System.out.println();
         }
---
+++ app/src/main/java/com/example/datastructurealgorithm/datastructure/nolinear/matrixdatastructure/_3dMatrix.java
@@ -0,0 +1,42 @@
+package com.example.datastructurealgorithm.datastructure.nolinear.matrixdatastructure;
+
+import java.util.Scanner;
+
+public class _3dMatrix {
+    final int planesCount;
+    final int rowsCount;
+    final int columnsCount;
+
+    public final int matrix[][][];
+
+    public _3dMatrix(int planes, int rows, int columns) {
+        this.planesCount = planes;
+        this.rowsCount = rows;
+        this.columnsCount = columns;
+        matrix = new int[planes][rows][columns];
+    }
+
+    public void addData() {
+        Scanner scanner = new Scanner(System.in);
+        for (int plane = 0; plane < planesCount; plane++) {
+            for (int row = 0; row < rowsCount; row++) {
+                for (int column = 0; column < columnsCount; column++) {
+                    System.out.print("plane[" + plane + "] row[" + row + "] - column[" + column + "] : ");
+                    matrix[plane][row][column] = scanner.nextInt();
+                }
+            }
+        }
+    }
+
+    public void printData() {
+        for (int[][] plane : matrix) {
+            System.out.println("plane :" + plane);
+            for (int[] row : plane) {
+                for (int value : row) {
+                    System.out.print(value + " ");
+                }
+                System.out.println();
+            }
+        }
+    }
+}
