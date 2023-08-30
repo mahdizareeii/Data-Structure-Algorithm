24143f81956919d5ba71449cbf8aeca9e57e4068
M.Zareei <m.zareei@jabama.com>
2023-08-30 11:39:22 +0330

    sorting algorithm

--- app/src/main/java/com/example/datastructurealgorithm/sortingalgorithm/TimSort.java
+++ app/src/main/java/com/example/datastructurealgorithm/sortingalgorithm/TimSort.java
@@ -1,10 +1,78 @@
 package com.example.datastructurealgorithm.sortingalgorithm;
 
+import java.util.Arrays;
+
 public class TimSort implements SortingStrategy {
 
     @Override
     public int[] sort(int[] array) {
-        //TODO
-        return array;
+        return timSort(array);
+    }
+
+    private int[] timSort(int[] arr) {
+        int n = arr.length;
+        int minRun = 32;
+
+        // Perform Insertion Sort on subarrays of size minRun
+        for (int i = 0; i < n; i += minRun) {
+            insertionSort(arr, i, Math.min(i + minRun - 1, n - 1));
+        }
+
+        // Merge subarrays using the Merge Sort approach
+        for (int size = minRun; size < n; size = 2 * size) {
+            for (int left = 0; left < n; left += 2 * size) {
+                int mid = left + size - 1;
+                int right = Math.min(left + 2 * size - 1, n - 1);
+                merge(arr, left, mid, right);
+            }
+        }
+        return arr;
+    }
+
+    private int[] insertionSort(int[] arr, int left, int right) {
+        for (int i = left + 1; i <= right; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= left && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+        return arr;
+    }
+
+    private int[] merge(int[] arr, int left, int mid, int right) {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        int[] leftArray = Arrays.copyOfRange(arr, left, left + n1);
+        int[] rightArray = Arrays.copyOfRange(arr, mid + 1, mid + 1 + n2);
+
+        int i = 0, j = 0, k = left;
+
+        while (i < n1 && j < n2) {
+            if (leftArray[i] <= rightArray[j]) {
+                arr[k] = leftArray[i];
+                i++;
+            } else {
+                arr[k] = rightArray[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            arr[k] = leftArray[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            arr[k] = rightArray[j];
+            j++;
+            k++;
+        }
+        return arr;
     }
 }
